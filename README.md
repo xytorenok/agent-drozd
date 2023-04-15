b0a1d55ebd8c781f0a6fa53a5326df3c7a0bff11
xytorenok <88131869+xytorenok@users.noreply.github.com>
2023-04-15 23:23:34 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# agent-drozd
+# Game-stackblitz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-vxhpj4)
---
+++ script.js
@@ -0,0 +1,119 @@
+const header = document.getElementById('header');
+const main = document.getElementById('main');
+const game = document.getElementById('game');
+const failed = document.getElementById('failed');
+
+
+
+
+const cactus = document.getElementById('cactus');
+const stat = document.getElementById('status');
+const life = document.getElementById('life');
+
+const distancePoint = document.getElementById('distance');
+const cruiser = document.getElementById('cruiser');
+const rocket = document.getElementById('rocket');
+const bayraktar = document.getElementById('bayraktar');
+const btnStartGame = document.getElementById('startgame');
+
+const btnJump = document.getElementById('btnjump');
+
+let lives = 2;
+let daysClean = 0;
+
+btnStartGame.onclick = startGame
+
+function startGame(){
+  main.hidden = false
+  header.hidden = true
+}
+
+btnJump.onclick = jump
+
+document.addEventListener('keydown', function (event) {
+  jump();
+});
+
+function jump() {
+  if (dino.classList != 'jump') {
+    dino.classList.add('jump');
+  }
+  setTimeout(function () {
+    dino.classList.remove('jump');
+  }, 600);
+
+  if (daysClean == 17) {
+    // убирает ракету после "попадания"
+    rocket.style.left = '150px'
+    rocket.style.animation = 'rocket 2000ms linear';
+  }
+
+  if (daysClean == 7) {
+    bayraktar.style.left = '-50px'
+    bayraktar.style.animation = 'bayraktarMov 8000ms linear';
+  }
+}
+
+let crash = false;
+let start = false;
+
+let isAlive = setInterval(function () {
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue('left')
+  );
+  life.innerHTML = lives;
+
+  //   счетчик пройденного пути
+  if (cactusLeft < 50 && cactusLeft > 0 && !start) {
+    start = true;
+    daysClean++;
+    distancePoint.innerHTML = daysClean;
+    // добавляем паузу в 1 секунду, чтобы счетчик не увеличивался слишком быстро
+    setTimeout(function () {
+      start = false;
+    }, 2000);
+  }
+
+  if (cactusLeft < 100 && cactusLeft > 50 && dinoTop >= 125 && !crash) {
+    crash = true;
+    lives--;
+    daysClean--;
+
+
+
+    // обнуляем пройденное растояние если жизни на нуле
+    if (lives == -1) {
+      lives = 2;
+      failed.hidden = false
+      daysClean = -1;
+      setTimeout(function() {
+        location.reload();
+      }, 3000);
+    }
+
+    // добавляем паузу в 1 секунду, чтобы счетчик не увеличивался слишком быстро
+    setTimeout(function () {
+      crash = false;
+    }, 2000);
+  }
+}, 10);
+
+let rocketStrike = setInterval(function () {
+
+
+let cruiserLeft = parseInt(window.getComputedStyle(cruiser).getPropertyValue('left'));
+let rocketLeft = parseInt(window.getComputedStyle(rocket).getPropertyValue('left'));
+ 
+  if (rocketLeft == cruiserLeft) {
+    lives = 666;
+    cruiser.style.opacity = '0';
+    cruiser.style.left = '140px'
+    cruiser.style.transition= 'all 1s linear';
+    rocket.style.opacity = '0';
+    rocket.style.transition= 'all 1s linear';
+  }
+
+}, 100);
+
+
